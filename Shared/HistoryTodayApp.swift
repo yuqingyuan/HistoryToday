1e1dd6a336d3f06ba9a05cd208576edc61247c08
qingyuanyu <qingyuanyu@tencent.com>
2020-06-23 14:27:48 +0800

    Initial Commit

---
+++ Shared/HistoryTodayApp.swift
@@ -0,0 +1,17 @@
+//
+//  HistoryTodayApp.swift
+//  Shared
+//
+//  Created by yuqingyuan on 2020/6/23.
+//
+
+import SwiftUI
+
+@main
+struct HistoryTodayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
